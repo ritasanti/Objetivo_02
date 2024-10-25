c2a39febe6be5ef4527b7b3c2a5fb4870f4004c5
lucianotb1 <lucianotb1@gmail.com>
2024-10-25 17:51:55 -0300

    ultimos cambios

--- main.py
+++ main.py
@@ -1,58 +1,68 @@
-from flask import Flask,jsonify,request
+from flask import Flask, jsonify, request
+from BD.db import conexion
 from models import modelo
-app=Flask(__name__)
+import hashlib
+from datetime import datetime
 
+app = Flask(__name__)
 
-#Endpoint para mostrar todas las personas de la tabla
+# Endpoint para mostrar todas las personas de la tabla
 @app.route("/persona", methods=["GET"])
-def get_Persona():
-    resultado=[]
-    result=modelo.obtenerDatos()
-    for personas in result:
-        resultado.append(personas)
+def get_persona():
+    resultado = []
+    result = modelo.obtenerDatos()
+    for persona in result:
+        resultado.append(persona)
     return jsonify(resultado)
 
-
-#Endpoint muestra datos que cumplen condicion
+# Endpoint para mostrar datos que cumplen condición
 @app.route("/persona/estado", methods=["GET"])
-def get_Persona_Estado():
+def get_persona_estado():
     return jsonify([persona for persona in modelo.obtenerDatos_Estado()])
 
+# API para agregar persona
+@app.route("/persona/agregar", methods=["POST"])
+def agregar_persona():
+    apellido = request.form.get("apellido")
+    nombres = request.form.get("nombres")
+    dni = request.form.get("dni")
+    domicilio = request.form.get("domicilio")
+    telefono = request.form.get("telefono")
+    fechora_registros = request.form.get("fechora_registros", datetime.today())
+    fecha_nac = request.form.get("fecha_nac")
+    genero = request.form.get("genero")
+    email = request.form.get("email")
+    id_reparticion = request.form.get("id_reparticion")
+    id_estado_registro = request.form.get("id_estado")
 
-#Ruta para el sp_RegistroPersona
-@app.route("/persona/SP_Registro", methods=["POST"])
-def inser_registro():
-    apellido=request.form.get("apellido")
-    nombres=request.form.get("nombres")
-    dni=request.form.get("dni")
-    domicilio=request.form.get("domicilio")
-    telefono=request.form.get("telefono")
-    id_estado=request.form.get("id_estado")
-    fechora_registro=request.form.get("fechora_registro")
     try:
-        modelo.sp_RegistroPersona(apellido,nombres,dni,domicilio,telefono,id_estado,fechora_registro)
-        return jsonify({"Mensaje":"Registro realizado correctamente"})
+        modelo.sp_AgregarPersona(apellido, nombres, dni, domicilio, fecha_nac, telefono, fechora_registros, genero, email, id_reparticion, id_estado_registro)
+        return jsonify({"Mensaje": "Registro realizado correctamente"}), 201
     except Exception as e:
-        return jsonify({"Mensaje":str (e)}), 500
+        return jsonify({"Mensaje": str(e)}), 500
 
-
-#Ruta para el SP_UpdatePersona
-@app.route("/persona/update/<int:id>", methods=["PUT"])
-def updatePersona(id):
+# API para modificar persona
+@app.route("/persona/modificar/<int:id>", methods=["PUT"])
+def modificar_persona(id):
     try:
-        apellido=request.form.get("apellido")
-        nombres=request.form.get("nombres")
-        dni=request.form.get("dni")
-        domicilio=request.form.get("domicilio")
-        telefono=request.form.get("telefono")
-        id_estado=request.form.get("id_estado")
-        modelo.sp_updatePersona(id,apellido,nombres,dni,domicilio,telefono,id_estado)
-        return jsonify({"Mensaje":"Se actualizo correctamente"})
+        apellido = request.form.get("apellido")
+        nombres = request.form.get("nombres")
+        dni = request.form.get("dni")
+        domicilio = request.form.get("domicilio")
+        telefono = request.form.get("telefono")
+        edad = request.form.get("edad")
+        genero = request.form.get("genero")
+        antiguedad = request.form.get("antiguedad")
+        email = request.form.get("email")
+        id_reparticion = request.form.get("id_reparticion")
+        id_estado = request.form.get("id_estado")
+        fechora_registro = datetime.now()
+        modelo.sp_updatePersona(id, apellido, nombres, dni, domicilio, telefono, edad, genero, antiguedad, email, id_reparticion, id_estado, fechora_registro)
+        return jsonify({"Mensaje": "Se actualizó correctamente"})
     except Exception as e:
-        return jsonify({"Mensaje":str (e)}), 500
+        return jsonify({"Mensaje": str(e)}), 500
 
-
-# Ruta para el SP_EliminarPersona
+# API para eliminar persona
 @app.route("/persona/eliminar/<int:id_persona>", methods=["DELETE"])
 def eliminar_persona(id_persona):
     try:
@@ -61,9 +71,120 @@ def eliminar_persona(id_persona):
     except Exception as e:
         return jsonify({"Error": str(e)}), 500
 
-# Ruta para SP_AgregarReparticion
+# Función para comparar contraseñas
+def verificar_claves(password_ingresada, hashed_password):
+    hash_ingresada = hashlib.md5(password_ingresada.encode()).hexdigest()
+    # Comparar claves
+    return hash_ingresada == hashed_password
+
+# API para comprobar que el usuario está registrado en la base de datos
+@app.route('/api/login', methods=['POST'])
+def login_api():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    con = conexion()
+    cursor = con.cursor()
+    try:
+        cursor.execute("SELECT id, password FROM usuarios WHERE nombres = %s", (username,))
+        user = cursor.fetchone()
+
+        if user and verificar_claves(password, user[1]):
+            return jsonify({'status': True, 'cod': user[0]})
+        else:
+            return jsonify({'status': False})
+    finally:
+        cursor.close()
+        con.close()
+
+# API para obtener tipo de usuario
+@app.route('/api/tipo_usuario', methods=['GET'])
+def obtener_tipo_usuario():
+    id_usuario = request.args.get('id_usuario')
+    if not id_usuario:
+        return jsonify({'status': False, 'error': 'Id_Usuario es requerido'}), 400
+
+    con = conexion()
+    cursor = con.cursor()
+    try:
+        query = """
+        SELECT tu.descripcion 
+        FROM usuarios u
+        JOIN tipo_usuario tu ON u.id_tipo_usuario = tu.id_tipo_usuario
+        WHERE u.id = %s
+        """
+        cursor.execute(query, (id_usuario,))
+        user = cursor.fetchone()
+
+        if user:
+            return jsonify({'status': True, 'tipo_usuario': user[0]})
+        else:
+            return jsonify({'status': False, 'error': 'Usuario no encontrado'}), 404
+    finally:
+        cursor.close()
+        con.close()
+
+# API para obtener datos del usuario
+@app.route('/api/datos_usuario', methods=['GET'])
+def obtener_datos_usuario():
+    id_usuario = request.args.get('id_usuario')
+    if not id_usuario:
+        return jsonify({'status': False, 'error': 'Id_Usuario es requerido'}), 400
+
+    con = conexion()
+    cursor = con.cursor()
+    try:
+        query = """
+        SELECT u.id, u.Apellido, u.Nombres, u.Password, tu.descripcion AS tipo_usuario, u.fechora_registro, u.fechora_modificacion, u.id_estado_registro
+        FROM usuarios u
+        JOIN tipo_usuario tu ON u.id_tipo_usuario = tu.id_tipo_usuario
+        WHERE u.id = %s
+        """
+        cursor.execute(query, (id_usuario,))
+        user = cursor.fetchone()
+
+        if user:
+            user_data = {
+                'id': user[0],
+                'Apellido': user[1],
+                'Nombres': user[2],
+                'Password': user[3],
+                'tipo_usuario': user[4],
+                'fechora_registro': user[5],
+                'fechora_modificacion': user[6],
+                'id_estado_registro': user[7]
+            }
+            return jsonify({'status': True, 'datos_usuario': user_data})
+        else:
+            return jsonify({'status': False, 'error': 'Usuario no encontrado'}), 404
+    finally:
+        cursor.close()
+        con.close()
+
+# API para consultar persona por ID
+@app.route("/persona/consulta/<int:id_persona>", methods=["GET"])
+def consulta_persona(id_persona):
+    try:
+        persona = modelo.sp_consultaPersonaID(id_persona)
+        if persona:
+            return jsonify(persona), 200
+        else:
+            return jsonify({"Mensaje": "Persona no encontrada"}), 404
+    except Exception as e:
+        return jsonify({"Error": str(e)}), 500
+
+# API para listar personas (limitadas a 30)
+@app.route("/persona/listado", methods=["GET"])
+def listado_persona():
+    num_personas = request.args.get('take', default=30, type=int)
+    result = modelo.obtenerDatos()
+    resultado = result[:num_personas]
+    return jsonify(resultado)
+
+# Ruta para agregar repartición
 @app.route("/reparticion/agregar", methods=["POST"])
-def agregarReparticion():
+def agregar_reparticion():
     try:
         nombres = request.form.get("nombres")
         descripcion = request.form.get("descripcion")
@@ -72,9 +193,9 @@ def agregarReparticion():
     except Exception as e:
         return jsonify({"Error": str(e)}), 500
 
-# Ruta para SP_ConsultaReparticionID
+# Ruta para consultar repartición por ID
 @app.route("/reparticion/consulta/<int:id_reparticion>", methods=["GET"])
-def consultaReparticion(id_reparticion):
+def consulta_reparticion(id_reparticion):
     try:
         reparticion = modelo.sp_consultaReparticionID(id_reparticion)
         if reparticion:
@@ -84,18 +205,18 @@ def consultaReparticion(id_reparticion):
     except Exception as e:
         return jsonify({"Error": str(e)}), 500
 
-# Ruta para SP_EliminarReparticion
+# Ruta para eliminar repartición
 @app.route("/reparticion/eliminar/<int:id_reparticion>", methods=["DELETE"])
-def eliminarReparticion(id_reparticion):
+def eliminar_reparticion(id_reparticion):
     try:
         modelo.sp_eliminarReparticion(id_reparticion)
         return jsonify({"Mensaje": f"Repartición con ID {id_reparticion} eliminada correctamente"}), 200
     except Exception as e:
         return jsonify({"Error": str(e)}), 500
 
-# Ruta para SP_ModificarReparticion
+# Ruta para modificar repartición
 @app.route("/reparticion/update/<int:id_reparticion>", methods=["PUT"])
-def modificarReparticion(id_reparticion):
+def modificar_reparticion(id_reparticion):
     try:
         nombres = request.form.get("nombres")
         descripcion = request.form.get("descripcion")
@@ -103,7 +224,37 @@ def modificarReparticion(id_reparticion):
         return jsonify({"Mensaje": "Repartición modificada correctamente"}), 200
     except Exception as e:
         return jsonify({"Error": str(e)}), 500
-    
-if __name__=='__main__':
+
+@app.route("/licencia/agregar", methods=["POST"])
+def agregar_licencia():
+    try:
+        # Extraer datos del formulario
+        p_Id_Persona = request.form.get("id_persona")
+        p_Nro_Art = request.form.get("nro_art")
+        p_Codigo = request.form.get("codigo")
+        p_Diagnostico = request.form.get("diagnostico")
+        p_Medico = request.form.get("medico")
+        p_Matricula = request.form.get("matricula")
+        p_Establecimiento = request.form.get("establecimiento")
+        p_Fecha_de_inicio = request.form.get("fecha_de_inicio")
+        p_Fecha_de_fin = request.form.get("fecha_de_fin")
+        p_Cant_dias_licencias = request.form.get("cant_dias_licencias")
+        p_Id_Estado_Licencia = request.form.get("id_estado_licencia")
+        p_Observaciones = request.form.get("observaciones")
+
+        # Llamar al modelo para insertar la licencia
+        mensaje = modelo.SP_InsertarLicencia(
+            p_Id_Persona, p_Nro_Art, p_Codigo, p_Diagnostico,
+            p_Medico, p_Matricula, p_Establecimiento,
+            p_Fecha_de_inicio, p_Fecha_de_fin,
+            p_Cant_dias_licencias, p_Id_Estado_Licencia,
+            p_Observaciones
+        )
+        return jsonify({"Mensaje": mensaje}), 201
+    except Exception as e:
+        return jsonify({"Error": str(e)}), 500
+
+
+if __name__ == '__main__':
     with app.app_context():
-        app.run(host="localhost",port="5000",debug=True)
+        app.run(host="localhost", port="5000", debug=True)
--- models/modelo.py
+++ models/modelo.py
@@ -1,6 +1,6 @@
 from BD.db import conexion
 from mysql.connector import Error
-import datetime
+from datetime import datetime
 import mysql.connector
 
 
@@ -30,58 +30,283 @@ def obtenerDatos_Estado():
     return registros
 
 
-#utilizacion de SP_RegistroPersona
-def sp_RegistroPersona(apellido,nombres,dni,domicilio,telefono,id_estado,fechora_registro):
-    con=conexion()
-    cursor=con.cursor()
-    sql_quer="call sp_registropersona(%s,%s,%s,%s,%s,%s,%s)"
+#utilizacion de SP_AgeregarPersona
+def SP_RegistroPersona(ID_persona,Apellido, Nombres,DNI, Domicilio, Fecha_Nac, Telefono, FecHora_Registros,FecHora_Modificacion,Edad, Genero,Antiguedad, Email, Id_Reparticion, Id_Estado_Registro):
+    con = conexion()
+    cursor = con.cursor()
+    
+    sql_quer = "CALL SP_RegistroPersona(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    
     try:
-        sql_quer="call sp_registropersona(%s,%s,%s,%s,%s,%s,%s)"
-        fechora_registro=datetime.date.today()
-        cursor.execute(sql_quer,(apellido,nombres,dni,domicilio,telefono,id_estado,fechora_registro))
+        cursor.execute(sql_quer, (ID_persona,Apellido, Nombres,DNI, Domicilio, Fecha_Nac, Telefono, FecHora_Registros,FecHora_Modificacion,Edad, Genero,Antiguedad, Email, Id_Reparticion, Id_Estado_Registro))
         con.commit()
     except Exception as e:
-        print("Error al quere insertar un registro: ",e)
+        print("Error al querer insertar un registro:", {e})
         con.rollback()
     finally:
         cursor.close()
         con.close()
 
 #Interaccion con la base para usar el procedimiento almacenado
-def sp_updatePersona(id,apellido,nombres,dni,domicilio,telefono,id_estado,fechora_registro):
+def SP_EditarPersona(ID_persona,Apellido, Nombres,DNI, Domicilio, Fecha_Nac, Telefono, FecHora_Registros,FecHora_Modificacion,Edad, Genero,Antiguedad, Email, Id_Reparticion, Id_Estado_Registro):
+    con = None  # Inicializamos con None
+    cursor = None
     try:
-        con=conexion()
-        cursor=con.cursor()
-        cursor.callproc=('sp_updatepersona',(id,apellido,nombres,dni,domicilio,telefono,id_estado,fechora_registro))
+        con = conexion()  # Creamos la conexión
+        cursor = con.cursor()
+        cursor.callproc('SP_EditarPersona', (ID_persona,Apellido, Nombres,DNI, Domicilio, Fecha_Nac, Telefono, FecHora_Registros,FecHora_Modificacion,Edad, Genero,Antiguedad, Email, Id_Reparticion, Id_Estado_Registro))
         con.commit()
-        print("Datos actualizados")
+        print("Datos actualizados ")
     except Exception as e:
-        print("Error al querer actualizar los datos: ",e)
+        print("Error al querer actualizar los datos: ", {e})
     finally:
-        cursor.close();
-        con.close();
+        if cursor is not None:  # Cerramos solo si cursor fue creado
+            cursor.close()
+        if con is not None:  # Cerramos solo si con fue creado
+            con.close()
+
+
 
 # Función para conectarse a la base de datos y ejecutar SP_DeletePersona
-def sp_eliminarPersona(id_persona):
+import mysql.connector
+from mysql.connector import Error
+
+def SP_EliminarPersona(Id_persona):
+    con = None
+    cursor = None
     try:
         # Establecer la conexión a la base de datos
-        con = mysql.connector.connect(
-            host='localhost', #Colocar cada uno su respectivos datos de la bd
-            database='persona-forbit', #Colocar cada uno su respectivos datos de la bd
-            user='root',  #Colocar cada uno su respectivos datos de la bd
-            password=''  #Colocar cada uno su respectivos datos de la bd
-        )
-        if con.is_connected():
-            cursor = con.cursor()
-            # Llamar al procedimiento almacenado SP_DeletePersona
-            sql_query = "CALL SP_DeletePersona(%s)"
-            cursor.execute(sql_query, (id_persona,))
-            con.commit()
-            print(f"Persona con ID {id_persona} eliminada (baja lógica) exitosamente.")
+        con =conexion()
+        cursor = con.cursor()
+        # Llamar al procedimiento almacenado SP_EliminarPersona
+        sql_query = "CALL SP_EliminarPersona(%s)"
+        cursor.execute(sql_query, (Id_persona,))
+        con.commit()
+        print(f"Persona con ID {Id_persona} eliminada (baja lógica) exitosamente.")
     except Error as e:
-        print(f"Error al ejecutar SP_DeletePersona: {e}")
-        con.rollback()  # Revertir en caso de error
+        print(f"Error al ejecutar SP_EliminarPersona: {e}")
+        if con:  # Verifica si la conexión fue establecida
+            con.rollback()  # Revertir en caso de error
     finally:
-        if con.is_connected():
+        if con:  # Cierra la conexión solo si fue establecida
             cursor.close()
+            con.close()
+
+
+def SP_InsertarLicencia(p_Id_Persona, p_Nro_Art, p_Codigo, p_Diagnostico,p_Medico, p_Matricula, p_Establecimiento,p_Fecha_de_inicio, p_Fecha_de_fin, p_Cant_dias_licencias, p_Id_Estado_Licencia, p_Observaciones):
+    con = None  # Inicializamos la conexión como None
+    cursor = None
+    try:
+        # Establecer la conexión a la base de datos
+        con = conexion()
+        cursor = con.cursor()
+        # Llamar al procedimiento almacenado SP_InsertarLicencia
+        cursor.callproc('SP_InsertarLicencia', (
+            p_Id_Persona, p_Nro_Art, p_Codigo, p_Diagnostico,
+            p_Medico, p_Matricula, p_Establecimiento,
+            p_Fecha_de_inicio, p_Fecha_de_fin,
+            p_Cant_dias_licencias, p_Id_Estado_Licencia,
+            p_Observaciones
+        ))
+        con.commit()  # Confirmar la transacción
+        print("Licencia insertada exitosamente.")
+    except Exception as e:
+        print("Error al querer insertar la licencia: ", {e})
+    finally:
+        if cursor is not None:  # Cerramos solo si el cursor fue creado
+            cursor.close()
+        if con is not None:  # Cerramos solo si la conexión fue establecida
+            con.close()
+
+def SP_AgregarReparticion(p_Nombres, p_Descripcion):
+    con = None  # Inicializamos la conexión como None
+    cursor = None  # Inicializamos el cursor como None
+    try:
+        # Establecer la conexión a la base de datos
+        con = conexion()
+        cursor = con.cursor()
+        # Llamar al procedimiento almacenado SP_AgregarReparticion
+        cursor.callproc('SP_AgregarReparticion', (p_Nombres, p_Descripcion))
+        con.commit()  # Confirmar la transacción
+        print("Repartición agregada exitosamente.")
+    except Exception as e:
+        print("Error al querer agregar la repartición: ", {e})
+    finally:
+        if cursor is not None:  # Cerramos solo si el cursor fue creado
+            cursor.close()
+        if con is not None:  # Cerramos solo si la conexión fue establecida
+            con.close()
+
+def SP_ConsultaPersona():
+    con = None  # Inicializamos la conexión como None
+    cursor = None  # Inicializamos el cursor como None
+    try:
+        # Establecer la conexión a la base de datos
+        con = conexion()
+        cursor = con.cursor()
+        # Llamar al procedimiento almacenado SP_ConsultaPersona
+        cursor.callproc('SP_ConsultaPersona')
+        # Recoger los resultados
+        results = []
+        for result in cursor.stored_results():
+            results.extend(result.fetchall())  # Agregar resultados a la lista
+        # Devolver resultados
+        return results
+    except Exception as e:
+        print("Error al querer consultar personas: ", {e})
+        return None
+    finally:
+        if cursor is not None:  # Cerramos solo si el cursor fue creado
+            cursor.close()
+        if con is not None:  # Cerramos solo si la conexión fue establecida
+            con.close()
+
+def SP_ConsultaPersonaDNI(DNI_Persona):
+    con = None  # Inicializamos la conexión como None
+    cursor = None  # Inicializamos el cursor como None
+    try:
+        # Establecer la conexión a la base de datos
+        con = conexion()
+        cursor = con.cursor()
+        # Llamar al procedimiento almacenado SP_ConsultaPersonaDNI
+        cursor.callproc('SP_ConsultaPersonaDNI', (DNI_Persona,))
+
+        # Recoger los resultados
+        results = []
+        for result in cursor.stored_results():
+            results.extend(result.fetchall())  # Agregar resultados a la lista
+
+        # Devolver resultados
+        return results
+
+    except Exception as e:
+        print("Error al querer consultar persona por DNI: ", {e})
+        return None
+    finally:
+        if cursor is not None:  # Cerramos solo si el cursor fue creado
+            cursor.close()
+        if con is not None:  # Cerramos solo si la conexión fue establecida
+            con.close()
+
+def SP_ConsultaPersonaID(Id_Persona):
+    con = None  # Inicializamos la conexión como None
+    cursor = None  # Inicializamos el cursor como None
+    try:
+        # Establecer la conexión a la base de datos
+        con = conexion()
+        cursor = con.cursor()
+        # Llamar al procedimiento almacenado SP_ConsultaPersonaID
+        cursor.callproc('SP_ConsultaPersonaID', (Id_Persona,))
+        # Recoger los resultados
+        results = []
+        for result in cursor.stored_results():
+            results.extend(result.fetchall())  # Agregar resultados a la lista
+        # Devolver resultados
+        return results
+
+    except Exception as e:
+        print("Error al querer consultar persona por ID: ", {e})
+        return None
+    finally:
+        if cursor is not None:  # Cerramos solo si el cursor fue creado
+            cursor.close()
+        if con is not None:  # Cerramos solo si la conexión fue establecida
+            con.close()
+
+def SP_ConsultaPersonaLike(apellido):
+    con = None  # Inicializamos la conexión como None
+    cursor = None  # Inicializamos el cursor como None
+    try:
+        # Establecer la conexión a la base de datos
+        con = conexion()
+        cursor = con.cursor()
+        # Llamar al procedimiento almacenado SP_ConsultaPersonaLike
+        cursor.callproc('SP_ConsultaPersonaLike', (apellido,))
+        # Recoger los resultados
+        results = []
+        for result in cursor.stored_results():
+            results.extend(result.fetchall())  # Agregar resultados a la lista
+        # Devolver resultados
+        return results
+
+    except Exception as e:
+        print("Error al querer consultar persona por apellido: ", {e})
+        return None
+    finally:
+        if cursor is not None:  # Cerramos solo si el cursor fue creado
+            cursor.close()
+        if con is not None:  # Cerramos solo si la conexión fue establecida
+            con.close()
+
+def SP_ConsultaReparticionID(Id_Reparticion):
+    con = None  # Inicializamos la conexión como None
+    cursor = None  # Inicializamos el cursor como None
+    try:
+        # Establecer la conexión a la base de datos
+        con = conexion()
+        cursor = con.cursor()
+        # Llamar al procedimiento almacenado SP_ConsultaReparticionID
+        cursor.callproc('SP_ConsultaReparticionID', (Id_Reparticion,))
+
+        # Recoger los resultados
+        results = []
+        for result in cursor.stored_results():
+            results.extend(result.fetchall())  # Agregar resultados a la lista
+
+        # Devolver resultados
+        return results
+
+    except Exception as e:
+        print("Error al querer consultar repartición por ID: ", {e})
+        return None
+    finally:
+        if cursor is not None:  # Cerramos solo si el cursor fue creado
+            cursor.close()
+        if con is not None:  # Cerramos solo si la conexión fue establecida
+            con.close()
+
+def SP_EliminarReparticion(Id_Reparticion):
+    con = None  # Inicializamos la conexión como None
+    cursor = None  # Inicializamos el cursor como None
+    try:
+        # Establecer la conexión a la base de datos
+        con = conexion()
+        cursor = con.cursor()
+        # Llamar al procedimiento almacenado SP_EliminarReparticion
+        cursor.callproc('SP_EliminarReparticion', (Id_Reparticion,))
+
+        con.commit()  # Confirmar los cambios
+        print(f"Repartición con ID {Id_Reparticion} eliminada (baja lógica) exitosamente.")
+
+    except Exception as e:
+        print("Error al querer eliminar la repartición: ", {e})
+        if con:  # Verifica si la conexión fue establecida
+            con.rollback()  # Revertir en caso de error
+    finally:
+        if cursor is not None:  # Cerramos solo si el cursor fue creado
+            cursor.close()
+        if con is not None:  # Cerramos solo si la conexión fue establecida
+            con.close()
+
+def SP_ModificarReparticion(Id_Reparticion, Nombres, Descripcion):
+    con = None  # Inicializamos la conexión como None
+    cursor = None  # Inicializamos el cursor como None
+    try:
+        # Establecer la conexión a la base de datos
+        con = conexion()
+        cursor = con.cursor()
+        # Llamar al procedimiento almacenado SP_ModificarReparticion
+        cursor.callproc('SP_ModificarReparticion', (Id_Reparticion, Nombres, Descripcion))
+
+        con.commit()  # Confirmar los cambios
+        print(f"Repartición con ID {Id_Reparticion} actualizada exitosamente.")
+
+    except Exception as e:
+        print("Error al querer modificar la repartición: ", {e})
+        if con:  # Verifica si la conexión fue establecida
+            con.rollback()  # Revertir en caso de error
+    finally:
+        if cursor is not None:  # Cerramos solo si el cursor fue creado
+            cursor.close()
+        if con is not None:  # Cerramos solo si la conexión fue establecida
             con.close()
